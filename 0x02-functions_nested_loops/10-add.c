d9374f7568982b8ece788e1f925a314ac6180164
Hafsalem <hafsalemdari@gmail.com>
2023-04-25 16:36:44 -0700

    10-add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
-#include <stdio.h>
+
 
 /**
- * Add - adds two values
+ * Add - Adds two values
  * @n1: The first number to be computed
  * @n2: The second number to be computed
  * Return: integer value
