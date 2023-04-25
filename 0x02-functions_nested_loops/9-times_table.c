6ca15317c7fb4c01326ed387c7b0629f79ec03b4
Hafsalem <hafsalemdari@gmail.com>
2023-04-25 14:39:39 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,7 +18,7 @@ void times_table(void)
 	if (z > 9)
 	{
 	u = z % 10;
-	d = (z -u) / 10;
+	d = (z - u) / 10;
 	_putchar(44);
 	_putchar(32);
 	_putchar(d + '0');
@@ -26,7 +26,7 @@ void times_table(void)
 	}
 	else
 	{
-	if ( y != 0)
+	if (y != 0)
 	{
 	_putchar(44);
 	_putchar(32);
