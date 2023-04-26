59f65273e014f9c9d0f9d0616024aa71fbf46591
Hafsalem <hafsalemdari@gmail.com>
2023-04-25 17:54:36 -0700

    Learn your times table

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,15 +2,19 @@
 
 #include <stdio.h>
 
+
+
 /**
- * print_to_98 - Prints all natural numbers from n to 98
- * @n: The number to start printing from
- * return: Always 0.
- */
+* print_to_98 - Prints all natural numbers from n to 98
+* @n: The number to start printing from
+* return: Always 0.
+*/
 
 void print_to_98(int n)
 
 {
+
+
 	if (n <= 98)
 	{
 	for (; n <= 98; n++)
@@ -21,13 +25,29 @@ void print_to_98(int n)
 	printf("\n");
 	break;
 	}
-        else
+	else
+	{
+	printf("%d, ", n);
+	}
+	}
+	}       
+	else
+	{
+	for (; n >= 98; n--)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
+	}
+	else
 	{
 	printf("%d, ", n);
 	}
 	}
 	}
-        else	
+	else
 	{
 	for (; n >= 98; n--)
 	{
