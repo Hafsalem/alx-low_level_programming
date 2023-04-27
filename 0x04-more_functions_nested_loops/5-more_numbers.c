7a111a29db89209a771d1dd9723f84305b2a220b
Hafsalem <hafsalemdari@gmail.com>
2023-04-27 13:08:22 -0700

    dd

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,14 @@ int x, y;
 
 for (x = 0; x < 10; x++)
 {
-for (y = 0; y < 14; y++)
+for (y = 0; y <= 14; y++)
 {
 if (y > 9)
 {
-putchar((y / 10) + '0');
+_putchar((y / 10) + '0');
 }
-putchar((y % 10) + '0');
+_putchar((y % 10) + '0');
 }
-putchar('\n');
+_putchar('\n');
 }
 }
