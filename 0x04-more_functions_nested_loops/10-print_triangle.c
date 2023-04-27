ac9b5feb0286887bf722a51aedfe75d70e6d9628
Hafsalem <hafsalemdari@gmail.com>
2023-04-27 13:12:42 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,29 +5,27 @@
  * @size: the size of the squares triangle
  * Return: empty
  */
-
 void print_triangle(int size)
-
 {
-	int x, y, z;
+int x, y, z;
 
-	if (size <= 0)
-	{
-	_putchar('\n');
-	}
-	else
-	{
-	for (x = 0; x < size; x++)
-	{
-	for (y = size - x; y > 1; y==)
-	{
-	_putchar(32);
-	}
-	for (z = 0; z <= x; z++)
-	{
-	_putchar(35);
-	}
-	_putchar('\n');
-	}
-	}
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (x = 0; x < size; x++)
+{
+for (y = size - x; y > 1; y==)
+{
+_putchar(32);
+}
+for (z = 0; z <= x; z++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
 }
