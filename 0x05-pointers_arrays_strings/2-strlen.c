bdfe35c97c6a199173fe552a4544db95176c8c6b
Hafsalem <hafsalemdari@gmail.com>
2023-05-03 07:00:34 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,7 @@
 /**
  * _strlen - returns the length of a string.
  * @str: the string to get the length of
- * return: the length of @str
+ * Return: the length of @str
  */
 size_t _strlen(const char *str)
 {
