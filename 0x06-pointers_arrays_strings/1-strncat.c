1618f93589442ea985cefd13e20159bd4cdfffbb
Hafsalem <hafsalemdari@gmail.com>
2023-05-04 05:06:56 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,6 +2,7 @@
 
 /**
  * _strncat - Concatenates two strings using at most
+ *
  * an inputted number of bytes from src.
  * @dest: the string to be appended upon.
  * @src: the string to be appended to dest.
